7744b7b4fe4708728c2adf3101ed01cb487277db
PallaviP1 <pallavi@mphatek.local>
2017-02-24 12:37:25 +0530

    1.4

--- Test2.go
+++ Test2.go
@@ -279,7 +279,9 @@ func (t *SimpleChaincode) createOrUpdateAsset(stub shim.ChaincodeStubInterface, 
 
     // Check if asset record existed in stub
     assetBytes, err:= stub.GetState(assetID)
-    if err != nil || len(assetBytes)==0{
+	stateStub = stateIn
+	fmt.Println("aasetbyte= ", assetBytes)
+   /* if err != nil || len(assetBytes)==0{
         // This implies that this is a 'create' scenario
          stateStub = stateIn // The record that goes into the stub is the one that cme in
     } else {
@@ -296,20 +298,20 @@ func (t *SimpleChaincode) createOrUpdateAsset(stub shim.ChaincodeStubInterface, 
             err = errors.New("Unable to merge state")
             return nil,err
         }
-    }
+    }*/
     stateJSON, err := json.Marshal(stateStub)
-    if err != nil {
+    /*if err != nil {
         return nil, errors.New("Marshal failed for contract state" + fmt.Sprint(err))
-    }
+    }*/
     // Get existing state from the stub
     
   
     // Write the new state to the ledger
     err = stub.PutState(assetID, stateJSON)
-    if err != nil {
+   /* if err != nil {
         err = errors.New("PUT ledger state failed: "+ fmt.Sprint(err))            
         return nil, err
-    } 
+    } */
     return nil, nil
 }
 /*********************************  internal: mergePartialState ****************************/	
